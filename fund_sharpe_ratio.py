a1f803b0b3e316f6d03d62ea20ef3044f810131c
lihao09 <lihao09@lihao09deMacBook-Pro.local>
2019-03-07 16:04:35 +0800

    修复爬取历史净值url失效的问题

--- fund_sharpe_ratio.py
+++ fund_sharpe_ratio.py
@@ -8,6 +8,47 @@ import os
 from config import *
 
 
+# 20190307 --- 之前爬取历史净值的url失效, 新增爬取逻辑
+def get_history_value_new(code, begin, fund_type):
+    file_name = "{}_fund_value_{}_{}.csv".format(fund_type, code, DATE_NOW)
+    if file_name in os.listdir(VALUE_DIR):
+        return
+
+    df_list = []
+    for page_num in range(1, 10000):
+        fund_url = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code={}&page={}&per=1000'.format(code, page_num)
+        soup = BeautifulSoup(urllib2.urlopen(url=fund_url), "lxml")
+        table = soup.find("table", {"class": "w782 comm lsjz"})
+        td_th = re.compile('t[dh]')
+        ret_list = []
+        for row in table.findAll("tr"):
+            cells = row.findAll(td_th)
+            row_data = dict()
+            if len(cells) == 7:
+                row_data[u'净值日期'] = cells[0].find(text=True)
+                row_data[u'单位净值'] = cells[1].find(text=True)
+                row_data[u'累计净值'] = cells[2].find(text=True)
+                row_data[u'日增长率'] = cells[3].find(text=True)
+            ret_list.append(row_data)
+
+        ret = pd.DataFrame(ret_list)
+        ret.drop(0, inplace=True)
+        ret = ret[ret[u'净值日期'].apply(lambda x: 1 if len(str(x).split("-")) == 3 else 0) > 0]
+        if ret.shape[0] <= 0:
+            break
+
+        ret[u'净值日期'] = pd.to_datetime(ret[u'净值日期'])
+        if ret[u'净值日期'].min() <= pd.to_datetime(begin):
+            df_list.append(ret[ret[u'净值日期'] >= pd.to_datetime(begin)])
+            break
+        else:
+            df_list.append(ret)
+    df_ret = pd.concat(df_list, axis=0)
+    print df_ret.shape
+    df_ret.to_csv(VALUE_DIR + file_name, index=False, encoding='utf-8')
+    return
+
+
 def get_history_value(code, begin, fund_type):
     file_name = "{}_fund_value_{}_{}.csv".format(fund_type, code, DATE_NOW)
     if file_name in os.listdir(VALUE_DIR):
@@ -99,7 +140,8 @@ def sr_rank_master(chose_type, top_rate, begin_date):
     chose_code_list = rr_rank_df['code'].tolist()[:int(rr_rank_df.shape[0] * top_rate)]
     sharpe_r_list = []
     for fund_code in chose_code_list:
-        get_history_value(fund_code, SHARPE_RATIO_BEGIN_DATE, chose_type)
+        # get_history_value(fund_code, SHARPE_RATIO_BEGIN_DATE, chose_type)
+        get_history_value_new(fund_code, begin_date, chose_type)
         sr_day, sr_week, sr_month = cal_sharpe_ratio(chose_type, fund_code, begin_date)
         sharpe_r_dict = dict()
         sharpe_r_dict['code'] = fund_code
@@ -122,4 +164,5 @@ def sr_rank_master(chose_type, top_rate, begin_date):
 
 
 if __name__ == '__main__':
-    sr_rank_master('zs', 0.2, '2016-04-01')
+    # sr_rank_master('zs', 0.2, '2016-04-01')
+    get_history_value_new('000248', '2018-01-01', 'zs')
