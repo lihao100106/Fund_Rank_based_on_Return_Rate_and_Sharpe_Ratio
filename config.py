44b31dab9e98ac39455e97e99bb1f8ccb24031fe
lihao56 <lihao56@guazi.com>
2020-02-15 11:42:53 +0800

    update

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import datetime
+import os
 
 
 FUND_TYPE = {'gp': u'股票型',
@@ -12,3 +13,10 @@ VALUE_DIR = 'data/history_Net_Asset_Value/'
 DATE_NOW = str(datetime.datetime.now())[:10]
 SHARPE_RATIO_BEGIN_DATE = '2014-01-01'
 RISK_FREE_GAIN = 1.03
+
+# 文件存储路径
+if not os.path.exists('data/'):
+    os.mkdir('data/')
+
+if not os.path.exists(VALUE_DIR):
+    os.mkdir(VALUE_DIR)
