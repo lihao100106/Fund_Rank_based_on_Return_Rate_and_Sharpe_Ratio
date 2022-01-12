0689a8feb273602342057128007d21d376a548be
Hecate2 <2474101468@qq.com>
2022-01-12 15:32:08 +0800

    multiprocessing for different types of funds

--- fund_select_main.py
+++ fund_select_main.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+from multiprocessing import Process
 from fund_sharpe_ratio import *
 from fund_return_rate import *
 from config import *
@@ -18,7 +19,13 @@ if __name__ == '__main__':
     #     rr_rank_master(chose_type=chose_type, time_list=['1y', '3y', '6y', '1n', '2n'])
 
     # 夏普率排名
-    chose_type = 'hh'
+    chose_type = {'gp': '股票型', 'hh': '混合型', 'zq': '债券型', 'zs': '指数型'}
     sr_top_rate = 0.05
     sr_begin_date = '2020-01-01'
-    sr_rank_master(chose_type=chose_type, top_rate=sr_top_rate, begin_date=sr_begin_date)
+    processes = [Process(target=sr_rank_master,
+                         kwargs={'chose_type': _type, 'top_rate': sr_top_rate, 'begin_date': sr_begin_date})
+                 for _type in chose_type]
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
